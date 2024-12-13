0e1a31a55f39d73c952f2023a4a6080c454eaeff
SangwooNa <105636611+SangwooNa@users.noreply.github.com>
2024-12-13 10:50:25 +0900

    Update app.py

--- app.py
+++ app.py
@@ -1,21 +1,22 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
 
 app = Flask(__name__)
+CORS(app)  # 모든 출처(origin) 허용
 
-PASSWORD = "your_password"  # 원하는 비밀번호 설정
+PASSWORD = "your_password"
 
 @app.route("/login", methods=["POST"])
 def login():
-    data = request.get_json()  # 클라이언트 요청에서 JSON 데이터를 받음
-    input_password = data.get("password")
-    if input_password == PASSWORD:
+    data = request.get_json()
+    if data.get("password") == PASSWORD:
         return jsonify({"success": True, "message": "Login successful!"})
     else:
         return jsonify({"success": False, "message": "Invalid password!"})
 
 @app.route("/")
 def home():
-    return "Flask app is running!"  # 기본 라우트로 앱 상태 확인 가능
+    return "Flask app is running!"
 
 if __name__ == "__main__":
     app.run(debug=True)
